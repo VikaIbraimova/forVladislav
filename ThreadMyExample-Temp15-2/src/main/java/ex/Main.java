4c3067c2b884702e759ff8d5f145209b2565b81f
vika <vika@mail.com>
2016-10-30 03:13:49 +0300

    Add MVC into my program

---
+++ ThreadMyExample-Temp15-2/src/main/java/ex/Main.java
@@ -0,0 +1,14 @@
+package ex;
+
+import ex.view.MainFrame;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        //FrameUsers fu = new FrameUsers();
+        MainFrame mainFrame = new MainFrame();
+    }
+}
---
+++ ThreadMyExample-Temp15-2/src/main/java/ex/model/MyData.java
@@ -0,0 +1,43 @@
+package ex.model;
+
+import ex.controller.MyDataExcel;
+
+public class MyData {
+    String col1;
+    String col2;
+    String col3;
+    MyDataExcel myDataExcel;
+
+    public MyData(String col1, String col2, String col3) {
+        this.col1 = col1;
+        this.col2 = col2;
+        this.col3 = col3;
+    }
+
+    public MyData() {
+    }
+
+    public void setCol1(String col1) {
+        this.col1 = col1;
+    }
+
+    public void setCol2(String col2) {
+        this.col2 = col2;
+    }
+
+    public void setCol3(String col3) {
+        this.col3 = col3;
+    }
+
+    public String getCol1() {
+        return col1;
+    }
+
+    public String getCol2() {
+        return col2;
+    }
+
+    public String getCol3() {
+        return col3;
+    }
+}
